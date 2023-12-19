8d86a08874893354d2594e4a98f3f1e2b6257f5e
FM-06 <90625121+FM-06@users.noreply.github.com>
2023-12-19 11:04:28 -0500

    Update FPT_CASINO.py

--- FPT_CASINO.py
+++ FPT_CASINO.py
@@ -8,7 +8,6 @@ pygame.font.init()
 #Variables
 background_colour = (0,0,0)
 
-
 #Start Screen
 StartScreen = pygame.display.set_mode((750,750))
 StartScreen.fill(background_colour)
@@ -17,7 +16,7 @@ pygame.display.flip()
 
 #Font
 TitleFont = pygame.font.SysFont("NeonFont.otf", 100)
-Title = TitleFont.render("PythonBets", True, (50,104,148))
+Title = TitleFont.render("PythonBets", True, (50,100,148))
 
 
 #Running variable to check if code should run
@@ -27,7 +26,11 @@ while screen == "START":
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
            screen = "END"
-        
-        pygame.draw.rect(StartScreen, (246,207,67), [180,85,425,100],0)
+        #Title
+        pygame.draw.rect(StartScreen, (246,207,67), [175,85,425,100],0)
         StartScreen.blit(Title, (200,100))
+
+        #Games Button
+        pygame.draw.rect(StartScreen, (246,207,67), pygame.Rect(240, 295, 300, 60),  2)  
+        #Wallet Button
         pygame.display.flip()
